a4d048adb2a5cff83e0697e7d020a464fe82798b
yasin yağmur <yasin.yagmur@gmail.com>
2022-10-26 22:41:25 +0300

    project repeat finished

--- Projects/tour-places/src/components/Main/Card.jsx
+++ Projects/tour-places/src/components/Main/Card.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Card = (item) => {
-  return <div>Card</div>;
+  const { id, title, image, desc } = item;
+  return (
+    <div className="cards">
+      <div className="title">
+        <h1>{title}</h1>
+      </div>
+      <img src={image} alt="" srcset="" />
+      <div className="card-over">
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
