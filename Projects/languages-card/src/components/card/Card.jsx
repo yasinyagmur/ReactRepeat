e7d6299b312dc3fe053fa78f69c4ae015c59e97c
yasin yağmur <yasin.yagmur@gmail.com>
2022-10-28 21:51:51 +0300

    three

--- Projects/languages-card/src/components/card/Card.jsx
+++ Projects/languages-card/src/components/card/Card.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { languages } from "../../helpers/data";
+import Item from "../item/Item";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="cards-area-container">
+      <div className="bars"></div>
+      <h1 className="languages-title">Languages</h1>
+      <div className="cards-container">
+        {languages.map((item, index) => {
+          //   return <h4>{item.name}</h4>;
+          return <Item card={item} key={index} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Card;
---
+++ Projects/languages-card/src/components/item/Item.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Item.css";
+
+const Item = (item) => {
+  console.log(item);
+  return <div>Item</div>;
+};
+
+export default Item;
