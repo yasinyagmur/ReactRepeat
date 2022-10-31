a6a5c7344865a2752621e3de75651723ad498bb1
yasin yağmur <yasin.yagmur@gmail.com>
2022-10-31 21:32:34 +0300

    use async await

--- Projects/tutorial-list/src/pages/Home.jsx
+++ Projects/tutorial-list/src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState();
+  const url = "https://tutorials-api-cw.herokuapp.com/api/tutorials";
+
+  const getTutorials = async () => {
+    const { data } = await axios.get(url);
+    // console.log(data);
+    setTutorials(data);
+  };
+  // getTutorials();
+
+  useEffect(() => {
+    getTutorials();
+  }, []);
+  console.log(tutorials);
   return (
     <div>
       <AddTutorial />
