54e01335bc768239bf16e57088ec4ca69458ee70
yasin yağmur <yasin.yagmur@gmail.com>
2022-11-01 22:44:40 +0300

    edit tutorial contiune

--- Projects/tutorial-list/src/components/EditTutorial.jsx
+++ Projects/tutorial-list/src/components/EditTutorial.jsx
@@ -1,32 +1,86 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-const EditTutorial = () => {
+const EditTutorial = ({ editTutorial, editItem }) => {
+  const { id, title: newTitle, description } = editItem;
+
+  const [title, setTitle] = useState(newTitle);
+  const [desc, setDesc] = useState(description);
+
+  //? https://reactjs.org/docs/hooks-reference.html#usestate
+  //! State degiskeninin degeri, 1.render ile initialState
+  //! parametresinin ilk degerini alir. Dolayisiyle bu durumda
+  //! prop'tan gelen ilk deger state'e aktarilir.
+  //! Sonradan degisen props degerleri useState'e aktarilmaz.
+  //! Eger props'tan gelen degerleri her degisimde useState'e
+  //! aktarmak istersek useEffect hook'unu componentDidUpdate
+  //! gibi kullanabiriz.
+
+  //? componentDidUpdate
+  //? newTitle veya description her degistiginde local title ve
+  //? desc state'lerimizi gunceliyoruz.
+  useEffect(() => {
+    setTitle(newTitle);
+    setDesc(description);
+  }, [newTitle, description]);
+
+  const handleSave = (e) => {
+    e.preventDefault();
+    editTutorial(id, title, desc);
+    setTitle("");
+    setDesc("");
+  };
+
   return (
-    <div class="modal" tabindex="-1">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Modal title</h5>
+    <div className="modal" tabIndex="-1" id="edit-modal">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Tutorial Update</h5>
             <button
               type="button"
-              class="btn-close"
+              className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
             ></button>
           </div>
-          <div class="modal-body">
-            <p>Modal body text goes here.</p>
+          <div className="modal-body">
+            <div className="mb-3">
+              <label htmlFor="title" className="form-label">
+                Title
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                placeholder="Enter your title"
+                value={title || ""}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="desc" className="form-label">
+                Description
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="desc"
+                placeholder="Enter your Description"
+                value={desc || ""}
+                onChange={(e) => setDesc(e.target.value)}
+                required
+              />
+            </div>
           </div>
-          <div class="modal-footer">
+          <div className="modal-footer">
             <button
               type="button"
-              class="btn btn-secondary"
+              className="btn btn-primary"
+              onClick={handleSave}
               data-bs-dismiss="modal"
             >
-              Close
-            </button>
-            <button type="button" class="btn btn-primary">
-              Save changes
+              Save
             </button>
           </div>
         </div>
