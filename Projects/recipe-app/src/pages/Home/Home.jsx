96cd48cdf8498f6eebd6b93a3a56d6cccab7ac1d
yasin yağmur <yasin.yagmur@gmail.com>
2022-11-11 23:39:48 +0300

    .env

--- Projects/recipe-app/src/pages/Home/Home.jsx
+++ Projects/recipe-app/src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import HomeStyle from "./Home.style";
 
 const Home = () => {
+  const APP_ID = process.env.REACT_APP_APP_ID;
+  const APP_KEY = process.env.REACT_APP_APP_KEY;
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
 
   return <div>Home</div>;
