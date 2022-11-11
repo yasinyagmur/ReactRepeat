550500f6bd9f1837328bab047a80bc52f721f395
yasin yağmur <yasin.yagmur@gmail.com>
2022-11-11 23:39:13 +0300

    url

--- Projects/recipe-app/src/pages/Home/Home.jsx
+++ Projects/recipe-app/src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import HomeStyle from "./Home.style";
 
 const Home = () => {
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${selectedMeal}`;
+
   return <div>Home</div>;
 };
 
