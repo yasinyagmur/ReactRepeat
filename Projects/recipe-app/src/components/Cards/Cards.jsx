bdcb31fb711f5c7f9c5a2f6ac39afdd6422c47f3
yasin yağmur <yasin.yagmur@gmail.com>
2022-11-12 12:30:11 +0300

    card components completed

--- Projects/recipe-app/src/components/Cards/Cards.jsx
+++ Projects/recipe-app/src/components/Cards/Cards.jsx
@@ -1,5 +1,26 @@
+import { Button, Card, Header, Image, MainContainer } from "./Cards.style";
+import defaultImage from "../../assets/default-image.jpg";
+import { Navigate, useNavigate } from "react-router-dom";
+
 const Cards = ({ recipes }) => {
-  return <div>Cards</div>;
+  const navigate = useNavigate();
+
+  return (
+    <MainContainer wrap="wrap">
+      {recipes.map(({ recipe }, index) => {
+        const { label, image } = recipe;
+        return (
+          <Card key={index}>
+            <Header>{label}</Header>
+            <Image src={image || defaultImage} />
+            <Button onClick={() => navigate("detail", { state: recipe })}>
+              View More
+            </Button>
+          </Card>
+        );
+      })}
+    </MainContainer>
+  );
 };
 
 export default Cards;
