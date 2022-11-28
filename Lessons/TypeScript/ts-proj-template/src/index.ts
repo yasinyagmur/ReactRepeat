b785ad29042dd3bc1c18fceb8d8235c2be608594
yasin yağmur <yasin.yagmur@gmail.com>
2022-11-28 21:45:55 +0300

    lesson one

--- Lessons/TypeScript/ts-proj-template/src/index.ts
+++ Lessons/TypeScript/ts-proj-template/src/index.ts
@@ -1,3 +1,24 @@
 export {};
 console.log("Hello TypeScript!");
 let a = 5;
+console.log(a);
+
+let myVar : any = "yasin";
+myVar = 89;
+console.log(myVar);
+
+let number : number[]=[];
+
+
+let v1 :unknown = 5;
+let v2: number= v1 as number;
+console.log(v1 , v2); 
+
+let firstName = "Yasin";
+let lastName ="jason";
+// void değer döndürmeyen 
+
+function getFullName(){
+    // console.log(firstName +"" + lastName);
+    // return(firstName +"" + lastName);
+} 
